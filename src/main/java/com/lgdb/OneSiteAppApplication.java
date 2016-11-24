eb95143610429f2443ebb58b290b6968fadbf432
Fredrik Viksten <fredrik.isak.viksten@gmail.com>
2016-11-24 10:36:31 +0100

    login page no database

---
+++ src/main/java/com/lgdb/OneSiteAppApplication.java
@@ -0,0 +1,61 @@
+package com.lgdb;
+
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.security.SecurityProperties;
+import org.springframework.cloud.netflix.zuul.EnableZuulProxy;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.core.annotation.Order;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.web.csrf.CookieCsrfTokenRepository;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.security.Principal;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.UUID;
+
+@SpringBootApplication
+@RestController
+@EnableZuulProxy
+public class OneSiteAppApplication {
+
+	@RequestMapping("/user")
+	public Principal user(Principal user) {
+		return user;
+	}
+
+
+	@RequestMapping("/resource")
+	public Map<String,Object> home() {
+		Map<String,Object> model = new HashMap<String,Object>();
+		model.put("id", UUID.randomUUID().toString());
+		model.put("content", "Hello World");
+		return model;
+	}
+
+
+	public static void main(String[] args) {
+		SpringApplication.run(OneSiteAppApplication.class, args);
+	}
+
+	@Configuration
+	@Order(SecurityProperties.ACCESS_OVERRIDE_ORDER)
+	protected static class SecurityConfiguration extends WebSecurityConfigurerAdapter {
+		@Override
+		protected void configure(HttpSecurity http) throws Exception {
+			http
+					.httpBasic()
+					.and()
+					.authorizeRequests()
+					.antMatchers("/index.html", "/home.html", "/login.html", "/", "/bower_components/**").permitAll()
+					.anyRequest().authenticated().and()
+					.csrf()
+					.csrfTokenRepository(CookieCsrfTokenRepository.withHttpOnlyFalse());
+		}
+	}
+
+}
